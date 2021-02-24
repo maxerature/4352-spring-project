04d043dcb2ec033b4bff8aea0698e4c589674524
lmalhaim <63025546+lmalhaim@users.noreply.github.com>
2021-02-24 00:19:15 -0600

    registration and profile management complete

--- Client/registration/registration.js
+++ Client/registration/registration.js
@@ -1,51 +1,13 @@
 
 
-
-const fs = require('fs')
-
-
 function register() {
     try{
-        let uname = document.querySelector(`#uname`).value || "Lynn"
-        let password = document.querySelector(`#pword`).value || "123"
-
-        fs.readFile('./users.json', (err, data) => {
-                if (err){
-                    console.log(alert("err = " + err.message))
-                }
-                var users_arr = JSON.parse(data)
-
-                if (users_arr[uname] == 0) {
-                    users_arr.users.push(
-                        {
-                            uname : {
-                                "password": password, 
-                                "full name":"",
-                                "address1":"", 
-                                "address2":"",
-                                "city":"", 
-                                "state":"", 
-                                "zipcode":""
-                            }
-                        }
-                    )
-
-                    fs.writeFile('./users.json', JSON.stringify(users_arr), 'utf-8',
-                        function(err){
-                            if (err){
-                                alert(err.message)
-                            }
-                            alert('Done!')
-                        }
-                    )
-                }else{
-                    alert("Username already taken.\n Please choose a different one")
-                }
-            }
-        )
+        let uname = document.querySelector(`#pword`).value
+        let password =document.querySelector(`#uname`).value
+        
     }
     catch(err){
-       alert("err = " + err.message)
+       alert("err1 = " + err.message)
     }
 }
 
@@ -53,15 +15,46 @@ function register() {
 
 function saveProfile(){
     try{
-        let uname = document.querySelector(`#uname`)
-        let password = document.querySelector(`#pword`)
-        let fullname = document.querySelector(`#fname`) + "_" + document.querySelector(`#lname`)
-        let add1 = document.querySelector(`#address1`)
-        let add2 = document.querySelector(`#address2`) 
-        let city = document.querySelector(`#city`)
-        let state = document.querySelector(`#state`)
-        let zipcode = document.querySelector(`#zipcode`)
+        let errors = 0
+        /*let uname = document.querySelector(`#uname`).value
+        let password = document.querySelector(`#pword`).value*/
+
+
+        let fullname = document.querySelector(`#fname`).value + "_" + document.querySelector(`#lname`).value
+        if (fullname.length == 1 || fullname.length > 50){
+            errors = errors + 1
+            alert("Please enter a valid name")
+        }
+
+        let add1 = document.querySelector(`#address1`).value
+        if (add1.length == 0 || add1.length  > 100){
+            errors = errors + 1
+            alert("Please enter a valid first address")
+        }
+
+        let add2 = document.querySelector(`#address2`).value
+
+        let city = document.querySelector(`#city`).value
+        if (city.length == 0 || city.length  > 100){
+            errors = errors + 1
+            alert("Please enter a valid city")
+        }
+
+        let state = document.querySelector(`#state`).value
+        if (state.length != 2){
+            errors = errors + 1
+            alert("Please select a state")
+        }
+
+
+        let zipcode = document.querySelector(`#zipcode`).value
+        if (zipcode.length < 5 || zipcode.length > 9){
+            errors = errors + 1
+            alert("zipcode must be between 5 and  7 characters")
+        }
+
+        
     }catch(err){
-        console.log(err.message)
+        alert(err.message)
     }
 }
