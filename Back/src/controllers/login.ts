bd94a66b96c3c3e10b51da77688854d6f57bd6ef
A-Baji <bidabaji@gmail.com>
2021-03-18 18:31:27 -0500

    unit tests now directly test controller methods

--- Back/src/controllers/login.ts
+++ Back/src/controllers/login.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import createError from "http-errors";
 import { AuthSchema } from "../config/Validation/auth";
-import { AuthLogin } from "../functions/login";
+import userInfo from "../../../Common/users.json";
+
+let userTS: any = userInfo;
 
 export default {
   login: async (
@@ -12,7 +14,30 @@ export default {
     try {
       await AuthSchema.validateAsync(req.body);
       const { username, password } = req.body;
-      res.json(AuthLogin(username, password));
+      // If username exists in db
+      if (username in userInfo.users) {
+        // If password matches in db
+        if (userTS.users[username].password == password) {
+          // If user already created profile
+          if (userTS.users[username].fullname != "") {
+            res.json({ success: "quote.html" });
+          } else {
+            res.json({ success: "ProfileManage.html" });
+          }
+        } else {
+          // error
+          res.json({
+            error:
+              "Error: Login failed\nUsername does not exist, or password was typed incorrectly.",
+          });
+        }
+      } else {
+        // error
+        res.json({
+          error:
+            "Error: Login failed\nUsername does not exist, or password was typed incorrectly.",
+        });
+      }
     } catch (error) {
       if (error.isJoi === true)
         return next(new createError.BadRequest("Invalid Username/Password"));
--- Back/src/unit-tests/tests.spec.ts
+++ Back/src/unit-tests/tests.spec.ts
@@ -1,55 +1,101 @@
-import { expect } from "chai";
-import "mocha";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import server from "../app";
 
-import { AuthLogin } from "../functions/login";
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+// Run test with: yarn run test
 
 describe("Login test", () => {
-  it("Successful login to the quote form", () => {
-    let username = "username1";
-    let password = "pass1";
-
-    let result = AuthLogin(username, password);
-    expect(result).to.include({ success: "quote.html" });
+  it("Successful login to the quote form", (done) => {
+    const body = {
+      username: "username1",
+      password: "pass1",
+    };
+    chai
+      .request(server)
+      .post("/login")
+      .send(body)
+      .end((err, res) => {
+        expect(res).have.status(200);
+        expect(res.body).to.eql({ success: "quote.html" });
+        done();
+      });
   });
 
-  it("Successful login to profile management", () => {
-    let username = "username2";
-    let password = "pass2";
-
-    let result = AuthLogin(username, password);
-    expect(result).to.include({ success: "ProfileManage.html" });
+  it("Successful login to profile management", (done) => {
+    const body = {
+      username: "username2",
+      password: "pass2",
+    };
+    chai
+      .request(server)
+      .post("/login")
+      .send(body)
+      .end((err, res) => {
+        expect(res).have.status(200);
+        expect(res.body).to.eql({ success: "ProfileManage.html" });
+        done();
+      });
   });
 
-  it("Failed login with existing username", () => {
-    let username = "username1";
-    let password = "pass12";
-
-    let result = AuthLogin(username, password);
-    expect(result).to.include({
-      error:
-        "Error: Login failed\nUsername does not exist, or password was typed incorrectly.",
-    });
+  it("Failed login with existing username", (done) => {
+    const body = {
+      username: "username1",
+      password: "pass12",
+    };
+    chai
+      .request(server)
+      .post("/login")
+      .send(body)
+      .end((err, res) => {
+        expect(res).have.status(200);
+        expect(res.body).to.eql({
+          error:
+            "Error: Login failed\nUsername does not exist, or password was typed incorrectly.",
+        });
+        done();
+      });
   });
 
-  it("Failed login with nonexisting username", () => {
-    let username = "username3";
-    let password = "pass1";
-
-    let result = AuthLogin(username, password);
-    expect(result).to.include({
-      error:
-        "Error: Login failed\nUsername does not exist, or password was typed incorrectly.",
-    });
+  it("Failed login with nonexisting username", (done) => {
+    const body = {
+      username: "username3",
+      password: "pass1",
+    };
+    chai
+      .request(server)
+      .post("/login")
+      .send(body)
+      .end((err, res) => {
+        expect(res).have.status(200);
+        expect(res.body).to.eql({
+          error:
+            "Error: Login failed\nUsername does not exist, or password was typed incorrectly.",
+        });
+        done();
+      });
   });
 
-  it("Failed login with empty username and password", () => {
-    let username = "";
-    let password = "";
-
-    let result = AuthLogin(username, password);
-    expect(result).to.include({
-      error:
-        "Error: Login failed\nUsername does not exist, or password was typed incorrectly.",
-    });
+  it("Failed login with empty username and password", (done) => {
+    const body = {
+      username: "",
+      password: "",
+    };
+    chai
+      .request(server)
+      .post("/login")
+      .send(body)
+      .end((err, res) => {
+        expect(res).have.status(400);
+        expect(res.body).to.eql({
+          error: {
+            message: "Invalid Username/Password",
+            status: 400,
+          },
+        });
+        done();
+      });
   });
 });
