31cd35e919ca02d102f08bcb5cdb6880cb7e6231
Lynn Alhaimy <63025546+lmalhaim@users.noreply.github.com>
2021-03-19 16:55:51 -0500

    Update register.ts

--- Back/src/controllers/register.ts
+++ Back/src/controllers/register.ts
@@ -3,6 +3,8 @@ import createError from "http-errors";
 import userInfo from "../../../Common/users.json";
 import { AuthSchema } from "../config/Validation/auth";
 
+let userlist:any = userInfo; 
+
 export default {
   register: async (
     req: express.Request,
@@ -10,33 +12,32 @@ export default {
     next: express.NextFunction
   ) => {
     try {
-      console.log("server arrive")
       await AuthSchema.validateAsync(req.body); 
       const {username, password} = req.body; 
-      if (username in userInfo.users) {
-        res.send({error: "username already in-use"})
-      }
-      try{
-        userInfo.users[username] = {
+      if (!(username in userlist.users)) {
+        userlist.users[username] = {
           "password": password,
-           "fullname": null,
-           "address1": null,
-           "address2": null,
-           "city": null,
-           "state": null,
-           "zipcode": null, 
-           "history": []
-        }; 
-        userInfo.users = userInfo.users; 
-        console.log(userInfo.users);
-      }catch(err){
-        console.log(err.message);
+            "fullname": "",
+            "address1": "",
+            "address2": "",
+            "city": "",
+            "state": "",
+            "zipcode": "", 
+            "history": []
+        }
+        res.json({ success: "user registered" })
+
+      }
+      else{
+        res.json({error: "username already in-use"})
       }
       
-        res.json({ success: "user registered" })
+      
     } catch (error) {
       if (error.isJoi === true) error.status = 422;
       next(error);
     }
   },
 };
+
+
