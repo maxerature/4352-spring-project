ad6e8c96e829082acc136ac694cb07d579c97121
lmalhaim <63025546+lmalhaim@users.noreply.github.com>
2021-03-21 20:26:52 -0500

    Revert "Delete Back directory"
    This reverts commit 48820b402a7e497f0d32e8d9823a10b049462d29.

---
+++ Back/src/controllers/register.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import createError from "http-errors";
+import userInfo from "../../../Common/users.json";
+import { AuthSchema } from "../config/Validation/auth";
+
+let userlist:any = userInfo; 
+
+export default {
+  register: async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      await AuthSchema.validateAsync(req.body); 
+      const {username, password} = req.body; 
+      if (!(username in userlist.users)) {
+        userlist.users[username] = {
+          "password": password,
+            "fullname": "",
+            "address1": "",
+            "address2": "",
+            "city": "",
+            "state": "",
+            "zipcode": "", 
+            "history": []
+        }
+        res.json({ success: "user registered" })
+
+      }
+      else{
+        res.json({error: "username already in-use"})
+      }
+      
+      
+    } catch (error) {
+      if (error.isJoi === true) error.status = 422;
+      next(error);
+    }
+  },
+};
---
+++ Back/src/routes/home.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import Config from '../config/enviormentVariables'
+import Settings from '../settings.json'
+import LoginController from '../controllers/login'
+import RegisterController from '../controllers/register'
+import MProfileController from '../controllers/manageProfile'
+import FuelQuoteController from '../controllers/fuelQuote'
+import FuelRequestController from '../controllers/fuelQuoteRequest'
+import FuelSubmitController from '../controllers/submitQuote'
+import HistoryController from '../controllers/history'
+
+
+export const HomeRoutes = express.Router()
+
+HomeRoutes.get('/', async (req, res, next) => {
+	res.send({
+		name: Settings.ProjectName,
+		version: Settings.ProjectVersion,
+		port: Config.server.PORT
+	})
+})
+
+HomeRoutes.post('/login', LoginController.login)
+
+HomeRoutes.post('/register', RegisterController.register)
+
+HomeRoutes.post('/manageProfile', MProfileController.manageProfile)
+
+HomeRoutes.post('/quoteLoad', FuelQuoteController.populate)
+
+HomeRoutes.post('/request', FuelRequestController.calculate)
+
+HomeRoutes.post('/submit', FuelSubmitController.submitQuote)
+
+HomeRoutes.post('/generateHistory', HistoryController.generateHistory)
+
+// Add routes using above syntax
