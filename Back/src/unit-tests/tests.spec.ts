e03b7628ac8403d90bf5c79c5b5cd0236a2af2f5
A-Baji <bidabaji@gmail.com>
2021-04-01 17:11:54 -0500

    Update tests.spec.ts

--- Back/src/unit-tests/tests.spec.ts
+++ Back/src/unit-tests/tests.spec.ts
@@ -120,7 +120,6 @@ describe("Login test", () => {
   });
 });
 
-
 describe("Registration test", () => {
   it("Failed registration with existing username", (done) => {
     const body = {
@@ -133,38 +132,37 @@ describe("Registration test", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({error: "username already in-use"});
+        expect(res.body).to.eql({ error: "username already in-use" });
         done();
       });
- });
- it("Successful registration with unique username", (done) => {
-  const body = {
-    username: "lmalhaim",
-    password: "pass123",
-  };
-  chai
-    .request(server)
-    .post("/register")
-    .send(body)
-    .end((err, res) => {
-      expect(res).have.status(200);
-      expect(res.body).to.eql({ success: "user registered" });
-      done();
-    });
+  });
+  it("Successful registration with unique username", (done) => {
+    const body = {
+      username: "lmalhaim",
+      password: "pass123",
+    };
+    chai
+      .request(server)
+      .post("/register")
+      .send(body)
+      .end((err, res) => {
+        expect(res).have.status(200);
+        expect(res.body).to.eql({ success: "user registered" });
+        done();
+      });
   });
 });
-
 
 describe("Manage Profile test", () => {
   it("Successful manage profile when username is first time logging-in", (done) => {
     const body = {
       username: "username2",
-        fullname: "Lia_Johnson",
-        add1: "510 richmond avenu",
-        add2: "apt 515",
-        city: "Houston",
-        state: "TX",
-        zipcode: 77006
+      fullname: "Lia_Johnson",
+      add1: "510 richmond avenu",
+      add2: "apt 515",
+      city: "Houston",
+      state: "TX",
+      zipcode: 77006,
     };
     chai
       .request(server)
@@ -174,7 +172,7 @@ describe("Manage Profile test", () => {
         expect(res).have.status(200);
         expect(res.body).to.eql({ success: "profile input added" });
         done();
-    });
+      });
   });
   it("Failed to manage profile when username doesnt exist/not logged in", (done) => {
     const body = {
@@ -184,7 +182,7 @@ describe("Manage Profile test", () => {
       add2: "apt 515",
       city: "Houston",
       state: "TX",
-      zipcode: 77006
+      zipcode: 77006,
     };
     chai
       .request(server)
@@ -192,9 +190,9 @@ describe("Manage Profile test", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({error:"username doesnt exist"});
+        expect(res.body).to.eql({ error: "username doesnt exist" });
         done();
-    });
+      });
   });
 });
 describe("QuoteLoadTest", () => {
@@ -215,8 +213,6 @@ describe("QuoteLoadTest", () => {
         done();
       });
   });
-
-
 
   it("Failed due to empty username", (done) => {
     const body = {
@@ -263,12 +259,12 @@ describe("QuoteLoadTest", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({ 
-          "addr1": "1600 Pennsylvania Avenue, N.W.",
-          "addr2": "",
-          "city": "Washington",
-          "state": "DC",
-          "zipCode": "20500"
+        expect(res.body).to.eql({
+          addr1: "1600 Pennsylvania Avenue, N.W.",
+          addr2: "",
+          city: "Washington",
+          state: "DC",
+          zipCode: "20500",
         });
         done();
       });
@@ -296,8 +292,8 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to missing parameters (pricePerGal)", (done) => {
     const body = {
-      deliveryDate:"10-2-2019", 
-      galsRequested:12
+      deliveryDate: "10-2-2019",
+      galsRequested: 12,
     };
     chai
       .request(server)
@@ -317,8 +313,8 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to missing parameters (galsRequested)", (done) => {
     const body = {
-      deliveryDate:"10-2-2019", 
-      pricePerGal: 12.03
+      deliveryDate: "10-2-2019",
+      pricePerGal: 12.03,
     };
     chai
       .request(server)
@@ -338,8 +334,8 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to missing parameters (deliveryDate)", (done) => {
     const body = {
-      galsRequested:12,
-      pricePerGal: 1
+      galsRequested: 12,
+      pricePerGal: 1,
     };
     chai
       .request(server)
@@ -359,7 +355,7 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to missing parameters (deliveryDate, pricePerGal)", (done) => {
     const body = {
-      galsRequested:12
+      galsRequested: 12,
     };
     chai
       .request(server)
@@ -379,7 +375,7 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to missing parameters (galsRequested, pricePerGal)", (done) => {
     const body = {
-      deliveryDate:"10-2-2019", 
+      deliveryDate: "10-2-2019",
     };
     chai
       .request(server)
@@ -399,7 +395,7 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to missing parameters (deliveryDate, galsRequested)", (done) => {
     const body = {
-      priceperGal: 2
+      priceperGal: 2,
     };
     chai
       .request(server)
@@ -419,9 +415,9 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to incorrect deliveryDate format", (done) => {
     const body = {
-      deliveryDate:"eleventeen", 
+      deliveryDate: "eleventeen",
       galsRequested: 10,
-      pricePerGal: 10
+      pricePerGal: 10,
     };
     chai
       .request(server)
@@ -429,16 +425,16 @@ describe("QuoteCalPriceTest", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({error: "Error. Wrong date format."});
+        expect(res.body).to.eql({ error: "Error. Wrong date format." });
         done();
       });
   });
 
   it("Failed due to incorrect gallons requested format", (done) => {
     const body = {
-      deliveryDate:"10-10-2019", 
+      deliveryDate: "10-10-2019",
       galsRequested: "a",
-      pricePerGal: 10
+      pricePerGal: 10,
     };
     chai
       .request(server)
@@ -458,9 +454,9 @@ describe("QuoteCalPriceTest", () => {
 
   it("Failed due to incorrect price per gallon format", (done) => {
     const body = {
-      deliveryDate:"10-10-2019", 
+      deliveryDate: "10-10-2019",
       galsRequested: 10,
-      pricePerGal: "a"
+      pricePerGal: "a",
     };
     chai
       .request(server)
@@ -480,9 +476,9 @@ describe("QuoteCalPriceTest", () => {
 
   it("Successfully returned a price", (done) => {
     const body = {
-      deliveryDate:"10-10-2019", 
+      deliveryDate: "10-10-2019",
       galsRequested: 10,
-      pricePerGal: 10
+      pricePerGal: 10,
     };
     chai
       .request(server)
@@ -490,12 +486,11 @@ describe("QuoteCalPriceTest", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({cost:100});
+        expect(res.body).to.eql({ cost: 100 });
         done();
       });
   });
 });
-
 
 describe("QuoteSubmitTest", () => {
   it("Failed due to no parameters", (done) => {
@@ -518,7 +513,7 @@ describe("QuoteSubmitTest", () => {
 
   it("Failed due to missing parameters (deliveryDate, galsRequested, pricePerGal, cost)", (done) => {
     const body = {
-      username: "foo"
+      username: "foo",
     };
     chai
       .request(server)
@@ -537,7 +532,7 @@ describe("QuoteSubmitTest", () => {
   });
   it("Failed due to missing parameters (username, galsRequested, pricePerGal, cost)", (done) => {
     const body = {
-      deliveryDate: "foo"
+      deliveryDate: "foo",
     };
     chai
       .request(server)
@@ -556,7 +551,7 @@ describe("QuoteSubmitTest", () => {
   });
   it("Failed due to missing parameters (username, deliveryDate, pricePerGal, cost)", (done) => {
     const body = {
-      galsRequested: 2
+      galsRequested: 2,
     };
     chai
       .request(server)
@@ -575,7 +570,7 @@ describe("QuoteSubmitTest", () => {
   });
   it("Failed due to missing parameters (username, deliveryDate, galsRequested, cost)", (done) => {
     const body = {
-      pricePerGal: 2
+      pricePerGal: 2,
     };
     chai
       .request(server)
@@ -594,7 +589,7 @@ describe("QuoteSubmitTest", () => {
   });
   it("Failed due to missing parameters (username, deliveryDate, galsRequested, pricePerGal)", (done) => {
     const body = {
-      cost: 2
+      cost: 2,
     };
     chai
       .request(server)
@@ -618,7 +613,7 @@ describe("QuoteSubmitTest", () => {
       deliveryDate: "10-10-2019",
       galsRequested: 10,
       pricePerGal: 10,
-      cost: 100
+      cost: 100,
     };
     chai
       .request(server)
@@ -626,7 +621,7 @@ describe("QuoteSubmitTest", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({failure: "You are not logged in."});
+        expect(res.body).to.eql({ failure: "You are not logged in." });
         done();
       });
   });
@@ -637,7 +632,7 @@ describe("QuoteSubmitTest", () => {
       deliveryDate: "10-10-2019",
       galsRequested: -10,
       pricePerGal: 10,
-      cost: 100
+      cost: 100,
     };
     chai
       .request(server)
@@ -645,7 +640,7 @@ describe("QuoteSubmitTest", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({error: "Gallons must be >0!"});
+        expect(res.body).to.eql({ error: "Gallons must be >0!" });
         done();
       });
   });
@@ -656,7 +651,7 @@ describe("QuoteSubmitTest", () => {
       deliveryDate: "10-10-2019",
       galsRequested: 10,
       pricePerGal: 10,
-      cost: 100
+      cost: 100,
     };
     chai
       .request(server)
@@ -666,14 +661,13 @@ describe("QuoteSubmitTest", () => {
         expect(res).have.status(200);
         expect(res.body).to.eql({
           success: "history.html",
-          string: JSON.stringify(userInfo)
+          string: JSON.stringify(userInfo),
           // string: `{"users":{"username1":{"password":"pass1","fullname":"fname1","address1":"1600 Pennsylvania Avenue, N.W.","address2":"","city":"Washington","state":"DC","zipcode":"20500","history":[{"requested":1.08,"delivery_address1":"The al'Thor Farm","delivery_address2":"","city":"Emond's Field","state":"Two Rivers","zipcode":"00000","delivery_date":"1245-06-22","suggested_ppg":1,"total":1.08},{"requested":420,"delivery_address1":"1600 Pennsylvania Avenue, N.W.","delivery_address2":"","city":"Washington","state":"DC","zipcode":"20500","delivery_date":"4-20-395","suggested_ppg":2,"total":840},{"requested":23,"delivery_address1":"1600 Pennsylvania Avenue, N.W.","delivery_address2":"","city":"Washington","state":"DC","zipcode":"20500","delivery_date":"2021-03-01","suggested_ppg":0.245,"total":5.635},{"requested":21,"delivery_address1":"1600 Pennsylvania Avenue, N.W.","delivery_address2":"","city":"Washington","state":"DC","zipcode":"20500","delivery_date":"2021-03-02","suggested_ppg":0.763,"total":16.023},{"requested":10,"delivery_address1":"1600 Pennsylvania Avenue, N.W.","delivery_address2":"","city":"Washington","state":"DC","zipcode":"20500","delivery_date":"10-10-2019","suggested_ppg":10,"total":100}]},"username2":{"password":"pass2","fullname":"","address1":"","address2":"","city":"","state":"","zipcode":""}}}`
         });
         done();
       });
   });
 });
-
 
 describe("HistoryLoadTest", () => {
   it("Failed due to no username whatsoever", (done) => {
@@ -696,7 +690,7 @@ describe("HistoryLoadTest", () => {
 
   it("Failed due to incorrect username", (done) => {
     const body = {
-      username: "DEBUG_incorrect"
+      username: "DEBUG_incorrect",
     };
     chai
       .request(server)
@@ -704,14 +698,14 @@ describe("HistoryLoadTest", () => {
       .send(body)
       .end((err, res) => {
         expect(res).have.status(200);
-        expect(res.body).to.eql({failure: "You are not logged in."});
+        expect(res.body).to.eql({ failure: "You are not logged in." });
         done();
       });
   });
 
   it("Returned successfully", (done) => {
     const body = {
-      username: "username1"
+      username: "username1",
     };
     chai
       .request(server)
@@ -720,10 +714,10 @@ describe("HistoryLoadTest", () => {
       .end((err, res) => {
         expect(res).have.status(200);
         expect(res.body).to.eql({
-          success:"<h3>Quote #1</h3><b>requested:</b> 1.08<br><b>delivery_address1:</b> The al'Thor Farm<br><b>delivery_address2:</b> <br><b>city:</b> Emond's Field<br><b>state:</b> Two Rivers<br><b>zipcode:</b> 00000<br><b>delivery_date:</b> 1245-06-22<br><b>suggested_ppg:</b> 1<br><b>total:</b> 1.08<br><h3>Quote #2</h3><b>requested:</b> 420<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 4-20-395<br><b>suggested_ppg:</b> 2<br><b>total:</b> 840<br><h3>Quote #3</h3><b>requested:</b> 23<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 2021-03-01<br><b>suggested_ppg:</b> 0.245<br><b>total:</b> 5.635<br><h3>Quote #4</h3><b>requested:</b> 21<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 2021-03-02<br><b>suggested_ppg:</b> 0.763<br><b>total:</b> 16.023<br><h3>Quote #5</h3><b>requested:</b> 10<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 10-10-2019<br><b>suggested_ppg:</b> 10<br><b>total:</b> 100<br>"
+          success:
+            "<h3>Quote #1</h3><b>requested:</b> 1.08<br><b>delivery_address1:</b> The al'Thor Farm<br><b>delivery_address2:</b> <br><b>city:</b> Emond's Field<br><b>state:</b> Two Rivers<br><b>zipcode:</b> 00000<br><b>delivery_date:</b> 1245-06-22<br><b>suggested_ppg:</b> 1<br><b>total:</b> 1.08<br><h3>Quote #2</h3><b>requested:</b> 420<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 4-20-395<br><b>suggested_ppg:</b> 2<br><b>total:</b> 840<br><h3>Quote #3</h3><b>requested:</b> 23<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 2021-03-01<br><b>suggested_ppg:</b> 0.245<br><b>total:</b> 5.635<br><h3>Quote #4</h3><b>requested:</b> 21<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 2021-03-02<br><b>suggested_ppg:</b> 0.763<br><b>total:</b> 16.023<br><h3>Quote #5</h3><b>requested:</b> 10<br><b>delivery_address1:</b> 1600 Pennsylvania Avenue, N.W.<br><b>delivery_address2:</b> <br><b>city:</b> Washington<br><b>state:</b> DC<br><b>zipcode:</b> 20500<br><b>delivery_date:</b> 10-10-2019<br><b>suggested_ppg:</b> 10<br><b>total:</b> 100<br>",
         });
         done();
       });
-
   });
 });
