2e198c9f77dcc5b6c91ec0d981794a55c7996716
Max B <max@maxbenlily.com>
2021-04-01 16:16:51 -0500

    Updated fuelQuote.ts to use database

--- Back/src/controllers/fuelQuote.ts
+++ Back/src/controllers/fuelQuote.ts
@@ -12,21 +12,55 @@ export default {
         next: express.NextFunction
     ) => {
         try {
+            console.log("I'm in!");
             //If user exists
             await AuthSchema.validateAsync(req.body);
             const { username } = req.body;
-            if(username in userTS.users) {
-                let addr1Var = userTS.users[username].address1;
-                let addr2Var = userTS.users[username].address2;
-                let cityVar = userTS.users[username].city;
-                let stateVar = userTS.users[username].state;
-                let zipcodeVar = userTS.users[username].zipcode;
 
-                res.json({ addr1: addr1Var, addr2: addr2Var, city: cityVar, state: stateVar, zipCode: zipcodeVar});
-            }
-            else {
-                res.json({ error: "You are in an incorrect account."});
-            }
+            var mysql = require('mysql2');
+
+            console.log("past sql");
+            var con = mysql.createConnection({
+                host: "localhost",
+                user: "root",
+                password: "password",
+                port: 3306,
+                database: "softwareproject"
+            });
+
+            console.log("created connection");
+            let query = "SELECT\n \
+                address1,\n \
+                address2,\n \
+                city,\n \
+                state,\n \
+                zipcode\n \
+            FROM Addresses\n \
+            INNER JOIN Users ON Addresses.userID = Users.userID\n \
+            WHERE\n \
+                Addresses.active = true\n \
+                AND Users.username = \"" + username + "\";";
+            console.log(query);
+            con.connect(function(err) {
+                if (err) throw err;
+                console.log("Connected!");
+                con.query(query, function (err, result) {
+                    if (err) throw err;
+                    console.log("Result: " + result);
+                    if(result == null || result == '') {
+                        res.json({error: "You are in an incorrect account."});
+                    }
+                    else {
+                        console.log("Got return!");
+                        console.log(result);
+                        var hj = JSON.stringify(result);
+                        console.log(hj);
+                        res.json({ addr1: result[0].address1, addr2: result[0].address2, city: result[0].city, state: result[0].state, zipCode: result[0].zipcode});
+                    }
+                });
+            }); 
+
+
         } catch (error) {
             if(error.isJoi === true) {
                 return next(new createError.BadRequest("Error. Invalid Username"));
